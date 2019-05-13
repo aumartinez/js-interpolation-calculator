ab6a26e8b1011652b58a8824923b0468c8f9c2ad
Augusto Martinez <acm_asd@hotmail.com>
2019-05-13 11:48:25 -0400

    Create action.js

---
+++ js/action.js
@@ -0,0 +1,31 @@
+//JavaScript
+
+function run(){
+  let elem;
+  
+  elem = document.getElementById("action-btn");
+  elem.addEventListener("click", calculateValues, false);
+}
+
+function calculateValues(){
+  let X0;
+  let Y0;
+  let X1;
+  let Y1;
+  let X;
+  let Y;
+  let result;
+      
+  X0 = Number(document.getElementById("px0").value);
+  Y0 = Number(document.getElementById("py0").value);
+  X1 = Number(document.getElementById("px1").value);
+  Y1 = Number(document.getElementById("py1").value);
+  X = Number(document.getElementById("px").value);
+  
+  Y = ((X - X0) * (Y1 - Y0) / (X1 - X0)) + Y0;  
+  
+  result = document.getElementById("result");
+  result.value = Y;
+}
+
+window.onload = run;
